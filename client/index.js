b20b7cf355106ba4c378678d959247910b315c97
Anatoly Tsinker <anatoly.tsiner12@gmail.com>
2022-09-23 16:47:50 -0400

    updated

--- client/index.js
+++ client/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
-import history from './history'
-// import store from './store'
-import App from './app'
+import App from './components/App'
+
 
 ReactDOM.render(
   <Router history={history}>
     <App />
   </Router>,
-  document.getElementById('app')
+  document.getElementById('App')
 )
--- server/index.js
+++ server/index.js
@@ -1,24 +1,73 @@
-const { db } = require("./db");
-const PORT = process.env.PORT || 8080;
-const app = require("./app");
-const seed = require("../script/seed");
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const compression = require('compression')
 
-const init = async () => {
-  try {
-    if (process.env.SEED === "true") {
-      await seed();
-    } else {
-      await db.sync();
-    }
-    // start listening (and create a 'server' object representing our server)
-    const server = app.listen(PORT, () =>
-      console.log(`Mixing it up on port ${PORT}`)
-    );
-    // const io = require("socket.io")(server);
-    // require("./socket")(io);
-  } catch (ex) {
-    console.log(ex);
+const app = express()
+
+// logging middleware
+app.use(morgan('dev'))
+
+//  body parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// compression middleware
+app.use(compression())
+
+// routers
+app.use('/api', require('./api'))
+
+app.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+});
+
+// file serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// remaining requests with an extension (.js, .css, other) send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error(`File Could not be located: ${req.path}`)
+    err.status = 404
+    next(err)
+  } else {
+    next()
   }
-};
+})
 
-init();
+// send index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
+
+// error handlers
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Endpoint Server Error')
+})
+
+function bootStartApp() {
+  console.log('Creating Server: ' + (process.env.DATABASE_NAME || 'no name provided'))
+  if (require.main === module) {
+    return (startServer())
+  }
+}
+
+const startServer = () => {
+  // syncDb()
+  // start listening and creates a server object
+  return app.listen(process.env.PORT, () => {
+    console.log(`App initializing. Now running on Port ${process.env.PORT}`)
+  })
+}
+
+// const syncDb = async () => {
+//   await db.sync()
+// }
+
+const server = bootStartApp()
+
+module.exports = server
